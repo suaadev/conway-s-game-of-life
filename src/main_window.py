0797a78dd54d1502fea819563873c5d9bcd926d6
David Suarez <79332655+suaadev@users.noreply.github.com>
2026-08-06 22:23:57 -0500

    feat: time control and code improvements have been added

--- src/main_window.py
+++ src/main_window.py
@@ -7,31 +7,39 @@ from game_egine import GameEngine
 
 
 class MainWindow:
+    FPS_RATE = 60
+    MAX_WINDOW_MS_TO_SIMULATE = 200
+    MIN_WINDOW_MS_TO_SIMULATE = 10
+
     def __init__(self, width: int, heigth: int, game_engine: GameEngine):
-        self.FRAME_RATE = 60
+
         self.heigth = heigth
         self.width = width
         pygame.init()
-        pygame.display.set_caption("jhon conway's game of life")
+        pygame.display.set_caption("Conway's Game of Life")
         self.display = pygame.display.set_mode(
             (self.width, self.heigth), pygame.RESIZABLE
         )
         self.game_engine = game_engine
         self.clock = pygame.time.Clock()
-        self.board = Board(self.width, self.heigth, self.game_engine)
+        self.board = Board(self.game_engine, self.display)
 
     def run(self):
+
+        last_time = pygame.time.get_ticks()
+        current_window_to_simulate = self.MIN_WINDOW_MS_TO_SIMULATE
+
         while True:
             events = pygame.event.get()
             mouse_position = pygame.mouse.get_pos()
-            # print(mouse_position)
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+
                 if event.type == pygame.VIDEORESIZE:
                     size = pygame.display.get_window_size()
-                    print(f"resized: {size} ")
+
                     self.board.resize(size[0], size[1])
 
                 if event.type == pygame.MOUSEBUTTONUP:
@@ -51,6 +59,18 @@ class MainWindow:
                     if keys[pygame.K_b]:
                         self.board.switch_cell_border()
 
+                    if (
+                        keys[pygame.K_m]
+                        and current_window_to_simulate < self.MAX_WINDOW_MS_TO_SIMULATE
+                    ):
+                        current_window_to_simulate += 10
+
+                    if (
+                        keys[pygame.K_n]
+                        and current_window_to_simulate > self.MIN_WINDOW_MS_TO_SIMULATE
+                    ):
+                        current_window_to_simulate -= 10
+
             mouse_pressed = pygame.mouse.get_pressed()
 
             if mouse_pressed[0]:
@@ -60,10 +80,15 @@ class MainWindow:
                 mouse_position = pygame.mouse.get_pos()
                 self.board.set_cell_status(mouse_position[0], mouse_position[1], False)
 
-            if self.board.simulation:
+            current_time = pygame.time.get_ticks()
+            if (
+                self.board.simulation
+                and current_time - last_time >= current_window_to_simulate
+            ):
                 self.board.next_generation()
+                last_time = current_time
 
-            self.display.blit(self.board, (0, 0))
+            self.display.blit(self.board.surface, self.board.surface_rect)
 
             pygame.display.update()
-            self.clock.tick(self.FRAME_RATE)
+            self.clock.tick(self.FPS_RATE)
